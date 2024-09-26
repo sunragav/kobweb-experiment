1742a92158eba53cf39dfe025ec8389c138993e1
Sundara Raghavan Santhanam <sunragav@gmail.com>
2024-09-26 18:37:14 +0200

    flutter app to test the webview

---
+++ webview_in_flutter/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';  // ADD
+import 'package:webview_in_flutter/web_view_stack.dart';
+
+import 'ob_web_view_navigation_toolbar.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  // Add from here...
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadFlutterAsset('assets/www/index.html');
+  }
+  // ...to here.
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+        // Add from here...
+        actions: [
+          NavigationControls(controller: controller),
+        ],
+        // ...to here.
+      ),
+      body: WebViewStack(controller: controller),       // MODIFY
+    );
+  }
+}
